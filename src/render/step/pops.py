eab101fc938e18ef9f69ac02529e310e7c48d989
Erik Sandstedt <goosefraba19@gmail.com>
2018-08-23 13:14:39 +0200

    Some more adjustments.

--- src/render/step/pops.py
+++ src/render/step/pops.py
@@ -14,13 +14,12 @@ class PopsStep(RenderStep):
 				
 				color = get_system_color(ctx, system)				
 				if color:
-					#color = tuple([min(x+60,255) for x in color])
-					color = tuple([int(0.25*a + 0.75*b) for a,b in zip((256,256,256), color)])
+					color = tuple([int(0.8*a + 0.2*b) for a,b in zip(color, (0,0,0))])
 				else:
 					color = (255,255,255)
 
 				p = convert_position_to_point(ctx, system.pos)
-				r = int(1.5 * math.sqrt(pops / math.pi) + 0.5)
+				r = max(int(config["scale"] * math.sqrt(pops / math.pi)), 1)
 				ctx.draw.ellipse(
 					(p[0]-r,p[1]-r,p[0]+r, p[1]+r), 
 					fill=color,

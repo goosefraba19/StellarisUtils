0c8a282a9e41e61921ca1adc346b6bf57967f0e2
Erik Sandstedt <goosefraba19@gmail.com>
2018-09-21 12:41:16 +0200

    Refactored Voronoi step.
    Moved most of the logic to the Voronoi class. Removed manual rings.

--- stellaris/render/step/voronoi.py
+++ stellaris/render/step/voronoi.py
@@ -1,26 +1,25 @@
 import math, numpy, random
 import scipy.spatial 
 
+from collections import defaultdict, namedtuple
+
 from ..render import RenderStep
 from ..color import get_color
 from ..utils import convert_position_to_point
 
-class RegionsStep(RenderStep):
+
+
+class VoronoiStep(RenderStep):
 
     def __init__(self):
         super().__init__("voronoi")
 
     def run(self, ctx, config):
-        # the voronoi diagram is built from a list of vertices
-        vertices = []
-        system_indices = []
-        
+        vor = Voronoi()
+
         # add vertices for every system
-        index = 0
         for system in ctx.model.systems.values():
-            vertices.append(list(system.pos))
-            system_indices.append((system.id, index))
-            index += 1
+            vor.add_vertex(system.pos, system)
 
         # add vertices for every hyperlane
         hyperlanes = set([h for s in ctx.model.systems.values() for h in s.hyperlanes])
@@ -31,53 +30,71 @@ class RegionsStep(RenderStep):
                 c += 1
 
             for weight in [x/c for x in range(1,c)]:
-                vertices.append([
+                vertex = [
                     weight * hyperlane.src.pos[0] + (1 - weight) * hyperlane.dest.pos[0],
                     weight * hyperlane.src.pos[1] + (1 - weight) * hyperlane.dest.pos[1]
-                ])
-
+                ]
                 # tied to the nearest system
                 system = hyperlane.src if 0.5 < weight else hyperlane.dest
-                system_indices.append((system.id, index))
-                index += 1
+                vor.add_vertex(vertex, system)
 
-        # add vertices for rings
-        config_rings = config["rings"]
-        for ring in self._get_rings(ctx, config_rings):
+        vor.generate_rings(ctx, config["rings"])
+
+        vor.finalize()
+
+        ctx.data[config["output"]] = vor
+
+
+
+
+
+Region = namedtuple("Region", ["system", "vertices"])
+
+class Voronoi:
+    def __init__(self):
+        self._vertices = []
+        self._system_indices = defaultdict(list)
+        self._index = 0
+
+    def add_vertex(self, vertex, system=None):
+        self._vertices.append(list(vertex))
+        if system != None:
+            self._system_indices[system].append(self._index)
+        self._index += 1
+
+    def generate_rings(self, ctx, config):
+        rings = []
+        for group in self._get_system_groups(ctx):
+            rings += self._generate_rings(ctx, config, group)
+
+        for ring in rings:
             angle = 0
             while angle < 360:
                 vertex = [
                     ring["x"] + ring["r"] * math.cos(math.radians(angle)),
-                    ring["y"] + ring["r"] * math.sin(math.radians(angle))
+                    ring["y"] + ring["r"] * math.sin(math.radians(angle)),
                 ]
 
-                if config_rings["debug"]:
+                if config["debug"]:
                     point = convert_position_to_point(ctx, vertex)
-                    ctx.draw.point(point, fill=get_color(ctx, config_rings["debug_color"]))
-                
-                vertices.append(list(vertex))
+                    ctx.draw.point(point, fill=get_color(ctx, config["debug_color"]))
+
+                self.add_vertex(vertex)
 
                 angle += ring["s"]
-        
-        # build diagram from vertices
-        vor = scipy.spatial.Voronoi(numpy.array(vertices))
 
-        ctx.data[config["output"]] = Voronoi(ctx, vor, system_indices)
+    def finalize(self):
+        vor = scipy.spatial.Voronoi(numpy.array(self._vertices))
 
+        self.regions = []
+        for (system, indices) in self._system_indices.items():
+            regions = [vor.regions[vor.point_region[i]] for i in indices]
+            for region in regions:
+                if -1 not in region:
+                    vertices = [tuple(vor.vertices[i]) for i in region]
+                    self.regions.append(Region(system, vertices))
 
-
-    def _get_rings(self, ctx, config):
-        if config["autogen"]:
-            results = []
-            for group in self._get_connected_systems(ctx):
-                results += self._create_rings_for_group(ctx, config, group)
-            return results
-        else:
-            return config["list"]
-
-
-
-    def _get_connected_systems(self, ctx):
+    def _get_system_groups(self, ctx):
         # https://www.geeksforgeeks.org/connected-components-in-an-undirected-graph/
         systems = ctx.model.systems
         visited = dict([(s.id, False) for s in systems.values()])
@@ -99,9 +116,7 @@ class RegionsStep(RenderStep):
 
         return result
 
-
-
-    def _create_rings_for_group(self, ctx, config, group):
+    def _generate_rings(self, ctx, config, group):
         if len(group) < 2:
             return []
 
@@ -122,40 +137,21 @@ class RegionsStep(RenderStep):
             r_min = min(r_min, r)
             r_max = max(r_max, r)
 
+        padding = config["padding"]
+        spacing = config["spacing"]
+
+        def create(center, radius, spacing):
+            n = (2*math.pi*radius) / spacing
+            s = 360/n
+            return { 
+                "x": center[0],
+                "y": center[1],
+                "r": radius,
+                "s": s
+            }
+
         # create rings
-        padding = config["autogen_padding"]
-        spacing = config["autogen_spacing"]
-        results = [self._create_ring(c, r_max + padding, spacing)]
+        results = [create(c, r_max + padding, spacing)]
         if padding < r_min:
-            results.append(self._create_ring(c, r_min - padding, spacing))
+            results.append(create(c, r_min - padding, spacing))
         return results
-
-
-
-    def _create_ring(self, center, radius, spacing):
-        n = (2*math.pi*radius) / spacing
-        s = 360/n
-        return { 
-            "x": center[0],
-            "y": center[1],
-            "r": radius,
-            "s": s
-        }
-
-
-
-class Voronoi:
-    def __init__(self, ctx, vor, system_indices):
-        self.regions = []
-
-        for (system_id, i) in system_indices:
-            region = vor.regions[vor.point_region[i]]
-            if -1 not in region:
-                system = ctx.model.systems[system_id]
-                vertices = [tuple(vor.vertices[j]) for j in region]
-                self.regions.append(Region(system, vertices))
-
-class Region:
-    def __init__(self, system, vertices):
-        self.system = system
-        self.vertices = vertices
